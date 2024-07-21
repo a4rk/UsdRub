855313356c24b284e1e549757249926e907ceb8c
a4rk <a4rk@mail.ru>
2024-07-21 18:04:08 +0300

    add todo

--- EnterAValue.java
+++ EnterAValue.java
@@ -1,8 +1,32 @@
+import java.util.Scanner;
+
 public class EnterAValue {
     public static void main(String[] args) {
-
+        double courseUsd = 93.75, courseRub = 1 /courseUsd, usd, rub;
+        System.out.println("Выберите пару валют для конвертации");
+        System.out.println("<1> для пары USD/RUB" + "\n" +"<2> для пары RUB/USD");
+        int currencyPair = new Scanner(System.in).nextInt();
+        correctTheValue:
+            if (currencyPair == 1) {
+                System.out.println("Введите значение в USD");
+                usd = new Scanner(System.in).nextDouble();
+                rub = usd * courseUsd;
+                System.out.println(usd + " S" + " = " + rub + ' ' + "RUB");
+            } else if (currencyPair == 2) {
+                System.out.println("Введите значение в RUB");
+                rub = new Scanner(System.in).nextDouble();
+                usd = rub * courseRub;
+                System.out.println(rub + " RUB" + " = " + usd + " $");
+            }
+            else {
+                // TODO: continue correctTheValue;  должно перенаправить на указанную метку
+            }
     }
 }
-// TODO do something
-//fixme multiline:
-// - dfv
+
+
+
+
+//FIXME:
+// - значение курса валют брать с сервера
+// - добавить новые валюты
