4a9ac68ae93f0827b3953cea6b5b36f4cafa3d55
a4rk <a4rk@mail.ru>
2024-07-21 18:55:30 +0300

    it works, but courseUsd is local

--- EnterAValue.java
+++ EnterAValue.java
@@ -1,12 +1,15 @@
+import java.util.Objects;
 import java.util.Scanner;
 
 public class EnterAValue {
     public static void main(String[] args) {
         double courseUsd = 93.75, courseRub = 1 /courseUsd, usd, rub;
-        System.out.println("Выберите пару валют для конвертации");
-        System.out.println("<1> для пары USD/RUB" + "\n" +"<2> для пары RUB/USD");
-        int currencyPair = new Scanner(System.in).nextInt();
-        correctTheValue:
+        int currencyPair = 0;
+        String quitOrContinue = "";
+        do {
+            System.out.println("Выберите пару валют для конвертации:");
+            System.out.println("<1> для пары USD/RUB" + "\n" + "<2> для пары RUB/USD");
+            currencyPair = new Scanner(System.in).nextInt();
             if (currencyPair == 1) {
                 System.out.println("Введите значение в USD");
                 usd = new Scanner(System.in).nextDouble();
@@ -17,16 +20,20 @@ public class EnterAValue {
                 rub = new Scanner(System.in).nextDouble();
                 usd = rub * courseRub;
                 System.out.println(rub + " RUB" + " = " + usd + " $");
+            } else {
+                System.out.println("Введено неверное значение");
             }
-            else {
-                // TODO: continue correctTheValue;  должно перенаправить на указанную метку
-            }
+            System.out.println("\n" + "Для завершения введите <Q>");
+            System.out.println("Для продолжения введите <Пробел>");
+            quitOrContinue = new Scanner(System.in).nextLine();
+        } while (!Objects.equals(quitOrContinue, "q") && !Objects.equals(quitOrContinue, "Q"));
     }
 }
 
 
 
-
+//TODO: none
 //FIXME:
 // - значение курса валют брать с сервера
 // - добавить новые валюты
+// - исправить вылеты программы при неверном вводе значений
